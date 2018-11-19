8c5d9f7c9428a0d0d420f381fa4d082b0421cae0
huanongying1201 <569013687@qq.com>
2018-11-19 23:13:41 +0800

    去掉无效POJO

--- src/main/java/com/zjl/mongo/Grade.java
+++ src/main/java/com/zjl/mongo/Grade.java
@@ -2,29 +2,33 @@ package com.zjl.mongo;
 
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
-import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.UpdateResult;
 
 public class Grade {
 
-	public static void main( String[] args ) throws IllegalAccessException, InvocationTargetException, ClassNotFoundException, InstantiationException{
+	public static void main( String[] args ) {
     	MongoJDBC jdbc = new MongoJDBC();
         	 MongoCollection<Document> col = jdbc.getDb("grade");
-//        	 Bson unwind = Aggregates.unwind("$stars");
-//        	 Bson project = Aggregates.project(and(eq("_id",0),eq("stars",1)));
-//        	 Bson sort = Aggregates.sort(eq("stars.num",-1));
-//        	 Bson limit = Aggregates.limit(5);
-//        	 AggregateIterable<Document> aggregateIterable = col.aggregate(Arrays.asList(unwind,project,sort,limit));
-//             List<Document> docList = aggregateIterable.into(new ArrayList<>());
-//             for(Document doc : docList) {
-//            	 Document stars =  (Document) doc.get("stars");
-//            	 Double num = (Double) stars.get("num");
-//            	 System.out.println(num);
-//             }
+        	 Bson unwind = Aggregates.unwind("$stars");
+        	 Bson project = Aggregates.project(and(eq("_id",0),eq("stars",1)));
+        	 Bson sort = Aggregates.sort(eq("stars.num",-1));
+        	 Bson limit = Aggregates.limit(5);
+        	 AggregateIterable<Document> aggregateIterable = col.aggregate(Arrays.asList(unwind,project,sort,limit));
+             List<Document> docList = aggregateIterable.into(new ArrayList<>());
+             for(Document doc : docList) {
+            	 Document stars =  (Document) doc.get("stars");
+            	 Double num = (Double) stars.get("num");
+            	 System.out.println(num);
+             }
         	 Bson filter = and(eq("num",5),eq("stars.num",80));
         	 Bson update = Updates.set("stars.$.need",440);
         	 UpdateResult s = col.updateOne(filter, update);
